f792242091bf3eb920fe3ce9c29ff12372e31195
vinik1ng <102757784+vinik1ng@users.noreply.github.com>
2022-05-16 14:16:37 -0300

    Add files via upload

---
+++ whilee.js
@@ -0,0 +1,30 @@
+//exercicio 1
+
+let c = 1
+while ( c <= 100 ){
+    console.log(c)
+    c = c + 1
+}
+
+// exercicio 2
+
+let b = 50
+while ( b <= 100 ){
+    console.log(b)
+    b = b + 1
+}
+
+//exercicio 3
+
+let d = 10
+while (d >= 0){
+    console.log(c);
+     d = d - 1
+}
+
+//exercicio 4
+
+let f = 0
+while(f <= 20){
+    console.log(f)
+    f = f + 2}
